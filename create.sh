ed7607d1ef07ee33ba72e3e1aa0be525bdb719cf
Paul Von Zimmerman <pcvonz@gmail.com>
2016-04-19 18:42:03 -0700

    added bashrc stuff

--- create.sh
+++ create.sh
@@ -3,3 +3,12 @@ rm -rf ~/.vim || echo ".vim doesn't exist"
 rm  ~/.vimrc
 ln -s ~/vim/.vimrc ~/.vimrc || echo ".vimrc already exists"
 ln -s ~/vim/.vim ~/.vim || echo "Symbolic link already exists"
+
+while true; do
+    read -p "Append settings to your bashrc?" yn
+    case $yn in
+        [Yy]* ) echo "alias tmux=\"TERM=screen-256color-bce tmux\"" >> ~/.bashrc; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
