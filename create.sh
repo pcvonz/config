d2ab6f5a1ba12b8543abdff35fb5b9712d3472d5
Paul Von Zimmerman <vonzimp@c-67-201-248-35.reshall.wwu.edu>
2016-11-08 09:32:47 -0800

    Adding tmux conf

--- create.sh
+++ create.sh
@@ -3,6 +3,7 @@ rm -rf ~/.vim || echo ".vim doesn't exist"
 rm  ~/.vimrc
 ln -s ~/config/.vimrc ~/.vimrc || echo ".vimrc already exists"
 ln -s ~/config/.vim ~/.vim || echo "Symbolic link already exists"
+ln -s ~/config/.tmux.conf ~/.tmux.conf || echo "tmux conf already exists"
 
 while true; do
     read -p "Append settings to your bashrc?" yn
